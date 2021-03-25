5ed2eb3b58c6297ad23577d09d9a38695067f25e
sujit995 <sujitmishra402@gmail.com>
2021-03-25 10:43:37 +0530

    changes in about page

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -35,7 +35,7 @@ const About = () => {
                       </Button>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1FipwWGy82U5XDuzkEpir3zEF2wITItUM/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1CpWEn0LXyk47-3meEi2ANutUjbI94TZr/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
